79f453e62512801350de612ef2a9fb0002beffcf
fedhere <fedhere@gmail.com>
2016-09-13 12:58:12 -0400

    adding Api script and FOrmatting script

---
+++ Lab2_fb55/NYCweatherAPI.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import json
+import urllib2 
+import os
+import sys
+
+
+apikey = os.getenv("WEATHERAPIKEY")
+mode = "Json"
+units = "metric"
+city = sys.argv[1]
+
+url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&mode=%s&units=%s&cnt=7&APPID=%s"%(city, mode, units, apikey)
+
+response = urllib2.urlopen(url)
+data = response.read().decode("utf-8")
+json.loads(data)
+print (data)
