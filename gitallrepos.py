7b3d3187162347ac4e91bb4dd9ef23a74c55b3a5
Federica Bianco <fbianco@compute.cusp.nyu.edu>
2016-10-05 21:21:36 -0400

    setting up safety checks

--- gitallrepos.py
+++ gitallrepos.py
@@ -4,9 +4,22 @@ import sys
 
 tmp = pd.read_csv(sys.argv[1])[['GitHub Link','Net ID']]
 tmp.dropna(inplace=True)
+puidir = os.getenv("PUI2016")
+if puidir is None:
+	print ("make sure the env variable PUI2016 is set up")
+	sys.exit()
+
+if not os.path.isdir(puidir):
+	print ("make sure %s exists and is a directory"%pui2916)
+	sys.exit()
+
+os.system("cd %s"%pui2016)
+cwd = os.getcwd()
+print ("cwd")
+
 for i,n in enumerate(tmp['Net ID'].values):
         print (tmp['GitHub Link'].values[i]+"/PUI2016_"+n)
-import os
-for i,n in enumerate(tmp['Net ID'].values):
-        os.system ("git clone " + tmp['GitHub Link'].values[i]+"/PUI2016_"+n)
+
+#for i,n in enumerate(tmp['Net ID'].values):
+#        os.system ("git clone " + tmp['GitHub Link'].values[i]+"/PUI2016_"+n)
 
